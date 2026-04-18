91d5e0fa50f7831a3af4e4472a09f8827ee78292
Lulynx <a998915@whitecrow.com.cn>
2026-04-19 01:40:49 +0800

    update index

--- frontend/dist/assets/index.html.c6ef684b.js
+++ frontend/dist/assets/index.html.c6ef684b.js
@@ -25,9 +25,9 @@ const C = s(
   null,
   -1
 );
-const E = s("p", null, "v1.4.1", -1);
+const E = s("p", null, "v1.4.3 Beta33", -1);
 const h = r(
-  `<p align="center"><strong>Fork from</strong> 秋葉 <a href="https://github.com/Akegarasu/lora-scripts" target="_blank" rel="noopener noreferrer">aaaki/lora-scripts</a></p><p align="center"><strong>Modify By</strong> <a href="https://github.com/WhitecrowAurora/lora-rescripts" target="_blank" rel="noopener noreferrer">Lulynx</a></p><h3 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志" aria-hidden="true">#</a> 更新日志</h3><h4 id="v1-4-1" tabindex="-1"><a class="header-anchor" href="#v1-4-1" aria-hidden="true">#</a> v1.4.1</h4><ul><li>新增 SDXL、Anima、Newbie 显存峰值控制。</li><li>新增 SDXL 独立 Block Swap 设置面板。</li><li>新增 SDXL、Anima、Newbie 的 LoRA-FA 训练支持。</li><li>新增 SDXL、Anima、Newbie 的 VeRA 训练支持。</li><li>新增 Lulynx 实验核心 LISA 调度支持。</li></ul>`,
+  `<p align="center"><strong>Fork from</strong> 秋葉 <a href="https://github.com/Akegarasu/lora-scripts" target="_blank" rel="noopener noreferrer">aaaki/lora-scripts</a></p><p align="center"><strong>Modify By</strong> <a href="https://github.com/WhitecrowAurora/lora-rescripts" target="_blank" rel="noopener noreferrer">Lulynx</a></p><h3 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志" aria-hidden="true">#</a> 更新日志</h3><h4 id="v1-4-3-beta33" tabindex="-1"><a class="header-anchor" href="#v1-4-3-beta33" aria-hidden="true">#</a> v1.4.3 Beta33</h4><ul><li>新增 SDXL Rectified Flow（RF）训练支持。</li><li>新增 SDXL Flow 训练高级选项（OT 配对、时间采样、CFM）。</li><li>优化 update / update_cn 更新脚本下载性能，避免 PowerShell 进度条导致的限速。</li><li>更新脚本增加文本百分比进度显示。</li></ul>`,
   5
 );
 
