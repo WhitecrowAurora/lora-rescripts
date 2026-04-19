6032c43124d40f025e211cbcee453fbfe4ba06ce
Lulynx <a998915@whitecrow.com.cn>
2026-04-19 08:57:57 +0800

    add ui change support

--- frontend/dist/assets/index.html.c6ef684b.js
+++ frontend/dist/assets/index.html.c6ef684b.js
@@ -25,9 +25,9 @@ const C = s(
   null,
   -1
 );
-const E = s("p", null, "v1.4.3 Beta33", -1);
+const E = s("p", null, "v1.4.4", -1);
 const h = r(
-  `<p align="center"><strong>Fork from</strong> 秋葉 <a href="https://github.com/Akegarasu/lora-scripts" target="_blank" rel="noopener noreferrer">aaaki/lora-scripts</a></p><p align="center"><strong>Modify By</strong> <a href="https://github.com/WhitecrowAurora/lora-rescripts" target="_blank" rel="noopener noreferrer">Lulynx</a></p><h3 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志" aria-hidden="true">#</a> 更新日志</h3><h4 id="v1-4-3-beta33" tabindex="-1"><a class="header-anchor" href="#v1-4-3-beta33" aria-hidden="true">#</a> v1.4.3 Beta33</h4><ul><li>新增 SDXL Rectified Flow（RF）训练支持。</li><li>新增 SDXL Flow 训练高级选项（OT 配对、时间采样、CFM）。</li><li>优化 update / update_cn 更新脚本下载性能，避免 PowerShell 进度条导致的限速。</li><li>更新脚本增加文本百分比进度显示。</li></ul>`,
+  `<p align="center"><strong>Fork from</strong> 秋葉 <a href="https://github.com/Akegarasu/lora-scripts" target="_blank" rel="noopener noreferrer">aaaki/lora-scripts</a></p><p align="center"><strong>Modify By</strong> <a href="https://github.com/WhitecrowAurora/lora-rescripts" target="_blank" rel="noopener noreferrer">Lulynx</a></p><h3 id="更新日志" tabindex="-1"><a class="header-anchor" href="#更新日志" aria-hidden="true">#</a> 更新日志</h3><h4 id="v1-4-4" tabindex="-1"><a class="header-anchor" href="#v1-4-4" aria-hidden="true">#</a> v1.4.4</h4><ul><li>新增原版 UI 设置页中的整套 UI 切换入口。</li><li>新增社区 UI 的 GitHub 地址安装与切换支持。</li><li>修复原版 UI 设置页刷新后错位、侧边栏失效等问题。</li></ul>`,
   5
 );
 
